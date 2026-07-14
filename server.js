8c6ae28972eed82e261eb5c56010b330852cef2e
hassibur rahman <hassiburrahman@hassiburs-Mac-mini.local>
2026-07-15 00:09:51 +0600

    added route and controller and middleware and model and  working on these

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Initialize Express App
 const app = express()
 
+// Connect Database
 await connectDB()
 
 // Middleware
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> res.send("Server is running"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
